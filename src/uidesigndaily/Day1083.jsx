e61ca6ae8f5f01206c2dd5168f1a9f9d9708c497
parmeshS <parmesh@signy.io>
2020-06-12 22:27:52 +0530

    first commit

---
+++ src/uidesigndaily/Day1083.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+export default () => {
+    return (
+        <div className="bg-white px-12 py-16 rounded-lg w-1/5 shadow-lg ">
+            <h4 className="font-extrabold text-lg text-center tracking-wide">Login</h4>
+            <p className="text-purple-600 mt-2 text-center text-sm font-medium">Create an account</p>
+            <div className="flex flex-col mt-4">
+                <label className="font-extrabold text-sm ">Username</label>
+                <input type="text" placeholder="Enter your username" className="px-3 text-xs py-3 mt-1 border-gray-400 border-2 rounded-md" />
+            </div>
+            <div className="flex pt-2 flex-col mt-4">
+                <label className="font-extrabold text-sm ">Password</label>
+                <input type="password" placeholder="Enter your password" className="px-3 text-xs py-3 mt-1 border-gray-400 border-2 rounded-md" />
+            </div>
+            <button className="text-xs rounded-lg bg-gray-800 mt-6 w-full text-white font-medium px-6 py-3">Go to lesson</button>
+            <span className="flex items-center mt-8 ">
+                <input type="checkbox" value="Awe" />
+                <span className="text-gray-900 text-sm font-bold ml-2">Keep me logged in</span>
+            </span>
+            <p className="text-purple-600 mt-16 text-center text-sm font-medium cursor-pointer">Forgot password?</p>
+            <p className="text-gray-600 mt-2 text-center text-sm font-medium">&copy; 2001 - 2020 All Right Reserved</p>
+        </div>)
+}
---
+++ src/uidesigndaily/Day1103.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export default () => {
+    return (
+        <div className="bg-white p-8 rounded w-1/6 shadow-lg ">
+            <h4 className="font-extrabold tracking-wide">Jump back in!</h4>
+            <p className="text-gray-600 mt-6 text-sm font-medium">Course 54% complete</p>
+            <div className="rounded-full relative bg-gray-400 h-2 mt-2 w-full">
+                <div className="absolute bg-purple-500 shadow-md h-full rounded-full w-3/4"></div>
+            </div>
+            <p className="text-gray-900 mt-8 text-xs font-bold">CURRENT LESSON</p>
+            <p className="text-gray-900 mt-1 text-lg font-semibold">Objects in Javascript<br />(Challenge)</p>
+            <button className="text-xs rounded bg-orange-400 mt-4 font-bold px-6 py-2">Go to lesson</button>
+        </div>)
+}
