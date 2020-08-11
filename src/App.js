aba0d7fd99d36901ddb4c34d3316968e42ccbe03
parmeshS <parmesh@signy.io>
2020-08-12 00:58:47 +0530

    Add carbon design

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Day1152 from './uidesigndaily/Day1152.jsx';
 import Day1103 from './uidesigndaily/Day1103.jsx';
 import Day1144 from './uidesigndaily/Day1144.jsx';
 import Day1153 from './uidesigndaily/Day1153.jsx';
-
+import Kanban from './adamTutorial/Kanban'
+import Carbon from './adamTutorial/carbon/Index.jsx'
 function App() {
   return (
-    <div className="flex justify-center min-h-screen items-center">
-      {/* <Day1083 /> */}
-      {/* <Day1103 /> */}
-      {/* <Day1152 /> */}
-      {/* <Day1144 /> */}
-      <Day1153 />
-    </div>
+    <Carbon />
+    // <div className="flex justify-center min-h-screen items-center">
+    //   {/* <Day1083 /> */}
+    //   {/* <Day1103 /> */}
+    //   {/* <Day1152 /> */}
+    //   {/* <Day1144 /> */}
+    //   <Day1153 />
+    // </div>
   );
 }
 
---
+++ src/adamTutorial/Kanban.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Kanban extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
